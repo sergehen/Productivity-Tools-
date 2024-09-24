37a646710e4be64b75fec7824532c4e05aa7106c
sergehen <sergehenry948@gmail.com>
2024-09-24 14:45:14 -0500

    this is the first commit

---
+++ gpt.js
@@ -0,0 +1,9 @@
+document.getElementById('reservation-form').addEventListener('submit', function(e) {
+ e.preventDefault();
+ alert('Reservation submitted! We will confirm your booking soon.');
+});
+
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+ e.preventDefault();
+ alert('Message sent! We will get back to you shortly.');
+});
